9b37f2305240a0c5188e1665943190125d98fe60
Nicholas Edwards <edwards.nicholas1997@gmail.com>
2022-12-16 11:45:16 -0500

    Completed lab

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,15 @@
 ## code your solution here. 
+class Cat
+    attr_accessor :name
+    def name=(cat_name)
+        @name = cat_name
+    end
+    def meow
+        puts "meow!"
+    end
+end
+
+ixs_cat = Cat.new
+ixs_cat.name = "Rose"
+puts ixs_cat.name
+ixs_cat.meow
